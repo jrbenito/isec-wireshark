56df19ff4456ac2bb68e9dfe4feb588cf73b22d1
Josenivaldo Benito Jr <jrbenito@benito.qsl.br>
2015-05-07 19:51:17 -0300

    Reorganized code displaying simple ISEC frame data parsed
    Stubs for support short frames but will as it is today will
    raise exception due lack of message boundary check.
    
    ToDo:
          -parse data/sub-frame (ISECProgram/ISECMobile)
          -boundary check for short frame and proper display
    
    Signed-off-by: Josenivaldo Benito Jr <jrbenito@benito.qsl.br>

--- packet-isecnet.c
+++ packet-isecnet.c
@@ -18,20 +18,39 @@ static dissector_handle_t isecnet_handle;
 /* Protocol ISECnet (isn) */
 static int proto_isecnet = -1;
 
+static int hf_isn_shortfrm = -1;
+#define ISN_SFRM_SZ 1
 static int hf_isn_length = -1;
 #define ISN_LEN_SZ 1
 static int hf_isn_cmd = -1;
 #define ISN_CMD_SZ 1
-static int hf_isn_payload = -1;
+static int hf_isn_data = -1;
 static int hf_isn_chksum = -1;
 #define ISN_CHKS_SZ 1
 
 #define ISN_HDR_SZ (ISN_CHKS_SZ + ISN_LEN_SZ) /* header + footer size */
 
-static gint ett_isn = -1;
+static gint ett_isn = -1; /* ISECnet */
+static gint ett_isp = -1; /* ISECProgram */
+static gint ett_ism = -1; /* ISECMobile */
+
+static const value_string isn_frm_names[] = {
+	{ 0xF7, "Connection Heartbeat"},
+	{ 0xFE, "ACK" },
+	{ 0, NULL }
+};
+
+static const value_string isn_cmd_names[] = {
+	{ 0x94, "Alarm Connect"},
+	{ 0xB0, "Alarm Event"},
+	{ 0xE7, "ISECProgram Message" },
+	{ 0xE9, "ISECMobile Message" },
+	{ 0, NULL }
+};
 
 /* Reassemble SMPP TCP segments */
 static gboolean reassemble_isecnet_over_tcp = TRUE;
+
 /* Module Preferences */
 static gboolean isecnet_summary_in_tree = TRUE;
 
@@ -66,17 +85,46 @@ static guint get_isecnet_pdu_len(packet_info *pinfo _U_, tvbuff_t *tvb, int offs
 static int dissect_isecnet(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_)
 {
 	proto_item *ti = NULL;
+	proto_tree *isecnet_tree = NULL;
 
 	gint offset = 0;
-	guint8 isn_pl_length = tvb_get_guint8(tvb, 0)-1;
+
+	guint8 isn_frame_length = -1;
+	guint8 isn_data_length = -1;
+	guint8 isn_cmd = -1;
+
+	/* Extract message Header and determine data length */
+	isn_frame_length = tvb_reported_length(tvb); /* frame size */ 
+	isn_data_length = tvb_get_guint8(tvb, offset)-ISN_CMD_SZ; /* data is length - CMD */
+	offset++;
+	isn_cmd = tvb_get_guint8(tvb, offset);
+
 
 	col_set_str(pinfo->cinfo, COL_PROTOCOL, "ISECnet");
 	/* Clear out stuff in the info column */
 	col_clear(pinfo->cinfo,COL_INFO);
+	col_add_fstr(pinfo->cinfo, COL_INFO,"ISECNet, Frame=%d, Cmd=%X",isn_frame_length,isn_cmd);
 
 	if (tree) { /* we are being asked for details */
 
+		/* Add ISECnet branch to main tree  */
 		ti = proto_tree_add_item(tree, proto_isecnet, tvb, 0, -1, ENC_NA);
+		if (isecnet_summary_in_tree) {
+			proto_item_append_text(ti,", Frame: %d, Cmd: %x",
+					      isn_frame_length, isn_cmd);
+
+		}
+		isecnet_tree = proto_item_add_subtree(ti, ett_isn);
+
+		offset = 0;
+		proto_tree_add_item(isecnet_tree, hf_isn_length, tvb, offset, ISN_LEN_SZ, ENC_BIG_ENDIAN);
+		offset += ISN_LEN_SZ;
+		proto_tree_add_item(isecnet_tree, hf_isn_cmd, tvb, offset, ISN_CMD_SZ, ENC_BIG_ENDIAN);
+		offset += ISN_CMD_SZ;
+		proto_tree_add_item(isecnet_tree, hf_isn_data, tvb, offset, isn_data_length, ENC_BIG_ENDIAN);
+		offset += isn_data_length;
+		proto_tree_add_item(isecnet_tree, hf_isn_chksum, tvb, offset, ISN_CHKS_SZ, ENC_BIG_ENDIAN);
+
 
 	}
 
@@ -114,17 +162,21 @@ void proto_register_isecnet(void)
 {
 	/* List of header fields */
 	static hf_register_info hf_isn[] = {
+		{ &hf_isn_shortfrm,
+		    { "Short frame", "ISECnet.shortfrm",
+		      FT_UINT8, BASE_HEX, VALS(isn_frm_names),
+		      0x0, NULL, HFILL}},
 		{ &hf_isn_length,
 		    { "Length", "ISECnet.length",
 		      FT_UINT8, BASE_DEC, NULL, 0X0,
 		      NULL, HFILL }},
 		{ &hf_isn_cmd,
 		    { "Command", "ISECnet.cmd",
-		      FT_UINT8, BASE_HEX, NULL, 0x0,
-		      NULL, HFILL }},
-		{ &hf_isn_payload,
-		    { "Payload", "ISECnet.payload",
-		      FT_NONE, BASE_NONE, NULL, 0x0,
+		      FT_UINT8, BASE_HEX, VALS(isn_cmd_names),
+		      0x0, NULL, HFILL }},
+		{ &hf_isn_data,
+		    { "Data", "ISECnet.data",
+		      FT_BYTES, BASE_NONE, NULL, 0x0,
 		      NULL, HFILL}},
 		{ &hf_isn_chksum,
 		    { "Checksum", "ISECnet.chksum",
@@ -134,7 +186,9 @@ void proto_register_isecnet(void)
 
 	/* Setup protocol subtree array */
 	static gint *ett[] = {
-		&ett_isn
+		&ett_isn,
+		&ett_isp,
+		&ett_ism
 	};
 
 	module_t *isecnet_module;
