74eec24148ae24bc634129638a941bf66f5b1ddd
Josenivaldo Benito Jr <jrbenito@benito.qsl.br>
2015-05-07 14:24:24 -0300

    More on better displaying dissect packet:
    -Treatment for short frames
    -Register module preferences for customization
    
    Signed-off-by: Josenivaldo Benito Jr <jrbenito@benito.qsl.br>

--- packet-isecnet.c
+++ packet-isecnet.c
@@ -3,7 +3,10 @@
  */
 
 #include "config.h"
+
 #include <glib.h>
+
+#include <epan/prefs.h>
 #include <epan/packet.h>
 #include <epan/dwarf.h>
 #include <epan/dissectors/packet-tcp.h>
@@ -23,12 +26,14 @@ static int hf_isn_payload = -1;
 static int hf_isn_chksum = -1;
 #define ISN_CHKS_SZ 1
 
-#define ISN_HDR_SZ (ISN_CHKS_SZ + ISN_LEN_SZ)
+#define ISN_HDR_SZ (ISN_CHKS_SZ + ISN_LEN_SZ) /* header + footer size */
 
 static gint ett_isn = -1;
 
 /* Reassemble SMPP TCP segments */
 static gboolean reassemble_isecnet_over_tcp = TRUE;
+/* Module Preferences */
+static gboolean isecnet_summary_in_tree = TRUE;
 
 static guint get_isecnet_pdu_len(packet_info *pinfo _U_, tvbuff_t *tvb, int offset)
 {
@@ -36,12 +41,32 @@ static guint get_isecnet_pdu_len(packet_info *pinfo _U_, tvbuff_t *tvb, int offs
 
 	msg_len = (guint)tvb_get_guint8(tvb,offset) + ISN_HDR_SZ;
 
+	/* ISECnet has short frames for Ack, for instance 0xFE ack
+	 * and F7 heartbeat
+	 *
+         * ToDo: Evaluate if it worthwhile handle any len>203 as short 
+         *       frame.
+         *       Will handle only two well documented short frames for
+         *       now
+         */
+	if ((msg_len == 0xFE) || (msg_len == 0xF7)) {
+		/* short frames have only header */
+		msg_len = ISN_LEN_SZ; 
+	} 
+	else {  /* Normal (long) frame length is payload length (first 
+		 *  field) + header and footer size
+		 */
+		msg_len = (guint)tvb_get_guint8(tvb,offset) + ISN_HDR_SZ;
+	}
+
 	return msg_len;
 }
 
 /* Dissect the ISECnet frame */
 static int dissect_isecnet(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_)
 {
+	proto_item *ti = NULL;
+
 	gint offset = 0;
 	guint8 isn_pl_length = tvb_get_guint8(tvb, 0)-1;
 
@@ -50,19 +75,9 @@ static int dissect_isecnet(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, 
 	col_clear(pinfo->cinfo,COL_INFO);
 
 	if (tree) { /* we are being asked for details */
-		proto_item *ti = NULL;
-		proto_tree *isn_tree = NULL;
 
 		ti = proto_tree_add_item(tree, proto_isecnet, tvb, 0, -1, ENC_NA);
-		isn_tree = proto_item_add_subtree(ti, ett_isn);
 
-/*		proto_tree_add_item(isn_tree, hf_isn_length, tvb, offset, 1, ENC_BIG_ENDIAN);
-		offset++;
-		proto_tree_add_item(isn_tree, hf_isn_cmd, tvb, offset, 1, ENC_BIG_ENDIAN);
-		offset++;
-		proto_tree_add_item(isn_tree, hf_isn_payload, tvb, offset, isn_pl_length, ENC_BIG_ENDIAN);
-		offset += isn_pl_length;
-		proto_tree_add_item(isn_tree, hf_isn_chksum, tvb, offset, 1, ENC_BIG_ENDIAN);*/
 	}
 
 	return tvb_captured_length(tvb);
@@ -121,7 +136,9 @@ void proto_register_isecnet(void)
 	static gint *ett[] = {
 		&ett_isn
 	};
-	
+
+	module_t *isecnet_module;
+
 	proto_isecnet = proto_register_protocol (
 		"Intelbras Security Protocol", /* name       */
 		"ISECnet",          /* short name */
@@ -132,6 +149,14 @@ void proto_register_isecnet(void)
 
 	proto_register_field_array(proto_isecnet, hf_isn, array_length(hf_isn));
 	proto_register_subtree_array(ett, array_length(ett));
+
+	/* Register configuration preferences */
+	isecnet_module = prefs_register_protocol(proto_isecnet, NULL);
+	prefs_register_bool_preference(isecnet_module, "summary_in_tree",
+			"Show TCP summary in protocol tree",
+			"Whether the TCP summary line should be shown in the protocol tree",
+			&isecnet_summary_in_tree);
+
 }
 
 /*
