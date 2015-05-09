cf9808c66bb409dc6d5620a7639bf5c33d7742e3
Josenivaldo Benito Jr <jrbenito@benito.qsl.br>
2015-05-09 00:07:50 -0300

    ISECnet correctly parsed
    ToDo: Parse content of ISECnet, the content can be:
          => ISECProgram frame
          => ISECMobile frame
          => ISec subcomand/request/answer
    
    Signed-off-by: Josenivaldo Benito Jr <jrbenito@benito.qsl.br>

--- packet-isecnet.c
+++ packet-isecnet.c
@@ -29,24 +29,25 @@ static int hf_isn_chksum = -1;
 #define ISN_CHKS_SZ 1
 
 #define ISN_HDR_SZ (ISN_CHKS_SZ + ISN_LEN_SZ) /* header + footer size */
+#define ISN_SHORTFRM_SZ 1 /* minimum short frame length */
+#define ISN_FULLFRM_SZ  (ISN_HDR_SZ + ISN_CMD_SZ) /* minimum long frame length (LEN+CMD+CHKS) */
+#define ISN_MAXFRM_SZ 203 /* maximum length of a long frame based on what we have from docs */
 
 static gint ett_isn = -1; /* ISECnet */
 static gint ett_isp = -1; /* ISECProgram */
 static gint ett_ism = -1; /* ISECMobile */
 
-static const value_string isn_frm_names[] = {
-	{ 0xF7, "Connection Heartbeat"},
-	{ 0xFE, "ACK" },
-	{ 0, NULL }
-};
 
 static const value_string isn_cmd_names[] = {
 	{ 0x94, "Alarm Connect"},
 	{ 0xB0, "Alarm Event"},
 	{ 0xE7, "ISECProgram Message" },
 	{ 0xE9, "ISECMobile Message" },
+        { 0xF7, "Connection Heartbeat"},
+        { 0xFE, "ACK" },
 	{ 0, NULL }
 };
+#define UNKNOWN_STR "unknown"
 
 /* Reassemble SMPP TCP segments */
 static gboolean reassemble_isecnet_over_tcp = TRUE;
@@ -58,24 +59,19 @@ static guint get_isecnet_pdu_len(packet_info *pinfo _U_, tvbuff_t *tvb, int offs
 {
 	guint msg_len = 0;
 
-	msg_len = (guint)tvb_get_guint8(tvb,offset) + ISN_HDR_SZ;
+        msg_len = (guint)tvb_get_guint8(tvb,offset);
 
 	/* ISECnet has short frames for Ack, for instance 0xFE ack
 	 * and F7 heartbeat
-	 *
-         * ToDo: Evaluate if it worthwhile handle any len>203 as short 
-         *       frame.
-         *       Will handle only two well documented short frames for
-         *       now
          */
-	if ((msg_len == 0xFE) || (msg_len == 0xF7)) {
+	if (msg_len > ISN_MAXFRM_SZ) {
 		/* short frames have only header */
-		msg_len = ISN_LEN_SZ; 
+		msg_len = ISN_SHORTFRM_SZ; 
 	} 
 	else {  /* Normal (long) frame length is payload length (first 
 		 *  field) + header and footer size
 		 */
-		msg_len = (guint)tvb_get_guint8(tvb,offset) + ISN_HDR_SZ;
+		msg_len += ISN_HDR_SZ;
 	}
 
 	return msg_len;
@@ -93,40 +89,63 @@ static int dissect_isecnet(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, 
 	guint8 isn_data_length = -1;
 	guint8 isn_cmd = -1;
 
-	/* Extract message Header and determine data length */
-	isn_frame_length = tvb_reported_length(tvb); /* frame size */ 
-	isn_data_length = tvb_get_guint8(tvb, offset)-ISN_CMD_SZ; /* data is length - CMD */
-	offset++;
-	isn_cmd = tvb_get_guint8(tvb, offset);
-
-
+        /* set protocol column */
 	col_set_str(pinfo->cinfo, COL_PROTOCOL, "ISECnet");
 	/* Clear out stuff in the info column */
 	col_clear(pinfo->cinfo,COL_INFO);
-	col_add_fstr(pinfo->cinfo, COL_INFO,"ISECNet, Frame=%d, Cmd=%X",isn_frame_length,isn_cmd);
+
+	/* Extract message Header and determine data length */
+	isn_frame_length = tvb_reported_length(tvb); /* frame size */
 
 	if (tree) { /* we are being asked for details */
-
-		/* Add ISECnet branch to main tree  */
-		ti = proto_tree_add_item(tree, proto_isecnet, tvb, 0, -1, ENC_NA);
-		if (isecnet_summary_in_tree) {
-			proto_item_append_text(ti,", Frame: %d, Cmd: %x",
-					      isn_frame_length, isn_cmd);
-
-		}
-		isecnet_tree = proto_item_add_subtree(ti, ett_isn);
-
-		offset = 0;
-		proto_tree_add_item(isecnet_tree, hf_isn_length, tvb, offset, ISN_LEN_SZ, ENC_BIG_ENDIAN);
-		offset += ISN_LEN_SZ;
-		proto_tree_add_item(isecnet_tree, hf_isn_cmd, tvb, offset, ISN_CMD_SZ, ENC_BIG_ENDIAN);
-		offset += ISN_CMD_SZ;
-		proto_tree_add_item(isecnet_tree, hf_isn_data, tvb, offset, isn_data_length, ENC_BIG_ENDIAN);
-		offset += isn_data_length;
-		proto_tree_add_item(isecnet_tree, hf_isn_chksum, tvb, offset, ISN_CHKS_SZ, ENC_BIG_ENDIAN);
-
-
+	         /* Add ISECnet branch to main tree  */
+	         ti = proto_tree_add_item(tree, proto_isecnet, tvb, 0, -1, ENC_NA);
+	         isecnet_tree = proto_item_add_subtree(ti, ett_isn);
 	}
+
+	if (isn_frame_length == ISN_SHORTFRM_SZ) { /* short frame */
+
+               isn_cmd = tvb_get_guint8(tvb, offset);
+
+               col_set_str(pinfo->cinfo, COL_INFO, val_to_str_const((guint32) isn_cmd, isn_cmd_names,UNKNOWN_STR));
+               col_append_fstr(pinfo->cinfo, COL_INFO,", Frame=%d, Cmd=%X",isn_frame_length,isn_cmd);
+
+               if (tree) { /* we are being asked for details */
+                        if (isecnet_summary_in_tree) {
+                                 proto_item_append_text(ti,", Frame: %d, Cmd: %x", isn_frame_length,
+                                                        isn_cmd);
+                                 proto_tree_add_item(isecnet_tree, hf_isn_cmd, tvb, offset,
+                                                     ISN_CMD_SZ, ENC_BIG_ENDIAN);
+                                 offset += ISN_CMD_SZ;
+                        }
+               } 
+	}
+	else { /* long frame */
+               isn_data_length = tvb_get_guint8(tvb, offset)-ISN_CMD_SZ; /* data is length - CMD */
+               offset++;
+               isn_cmd = tvb_get_guint8(tvb, offset);
+
+               col_set_str(pinfo->cinfo, COL_INFO, val_to_str_const((guint32) isn_cmd, isn_cmd_names,UNKNOWN_STR));
+               col_append_fstr(pinfo->cinfo, COL_INFO,", Frame=%d, Cmd=%X, Data=%d",isn_frame_length,isn_cmd,isn_data_length);
+
+               if (tree) { /* we are being asked for details */
+
+                        if (isecnet_summary_in_tree) {
+			         proto_item_append_text(ti,", Frame: %d, Cmd: %x",
+				   	                isn_frame_length, isn_cmd);
+
+                        }
+
+                        offset = 0;
+                        proto_tree_add_item(isecnet_tree, hf_isn_length, tvb, offset, ISN_LEN_SZ, ENC_BIG_ENDIAN);
+                        offset += ISN_LEN_SZ;
+                        proto_tree_add_item(isecnet_tree, hf_isn_cmd, tvb, offset, ISN_CMD_SZ, ENC_BIG_ENDIAN);
+                        offset += ISN_CMD_SZ;
+                        proto_tree_add_item(isecnet_tree, hf_isn_data, tvb, offset, isn_data_length, ENC_BIG_ENDIAN);
+                        offset += isn_data_length;
+                        proto_tree_add_item(isecnet_tree, hf_isn_chksum, tvb, offset, ISN_CHKS_SZ, ENC_BIG_ENDIAN);
+               }
+        }
 
 	return tvb_captured_length(tvb);
 }
@@ -134,7 +153,8 @@ static int dissect_isecnet(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, 
 /**
  * The minimum size of a ISECnet packet is 3 bytes (Length, CMD and checksum),
  * and the maximum size is 203 bytes (per documents and 256 due length filed
- * limitation). Hence minimum fixed lentgh should be 2 bytes for tcp_dissect_pdu.
+ * limitation); also there is a short frame packet which has only one byte.
+ * Hence minimum fixed lentgh should be 1 bytes for tcp_dissect_pdu.
  *
  * Since header field Length is only one byte long, it will be present within a
  * single TCP segment and no issue will be raised by calling tcp_dissect_pdu with
@@ -148,7 +168,7 @@ static int dissect_isecnet_data(tvbuff_t *tvb, packet_info *pinfo, proto_tree *t
 {
 	tcp_dissect_pdus(tvb, pinfo, tree,
 			reassemble_isecnet_over_tcp, 
-			2,			    /* Length can be determined within 1st byte */
+			ISN_SHORTFRM_SZ,			    /* Length can be determined within 1st byte */
 			get_isecnet_pdu_len, 
 			dissect_isecnet, data);
 
@@ -164,7 +184,7 @@ void proto_register_isecnet(void)
 	static hf_register_info hf_isn[] = {
 		{ &hf_isn_shortfrm,
 		    { "Short frame", "ISECnet.shortfrm",
-		      FT_UINT8, BASE_HEX, VALS(isn_frm_names),
+		      FT_UINT8, BASE_HEX, VALS(isn_cmd_names),
 		      0x0, NULL, HFILL}},
 		{ &hf_isn_length,
 		    { "Length", "ISECnet.length",
